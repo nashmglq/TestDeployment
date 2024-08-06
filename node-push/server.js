f33188f9ad92e5bc78368d6425ce4ac4ced2eaee
nashmglq <nashmglq89@gmail.com>
2024-08-06 22:08:57 +0800

    deploy

---
+++ node-push/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const serverless = require("serverless-http");
+const app = express();
+const port = process.env.PORT || 5000;
+const routes = require("./route/route");
+
+// use this for POST methods
+app.use(express.urlencoded({extended: true}))  // Parse URL-encoded data into a JavaScript object
+app.use(express.static("./public")) // use this for css styling
+
+app.use("/", routes)
+
+app.listen(port, () => {
+  console.log(port);
+});
+
